30a9449dd86ca5cf020b54677ae63474d5da76e0
Kenney Chan <kchan10@gmail.com>
2023-07-28 14:00:52 -0700

    Revert "Revert "Add user authentication""
    This reverts commit 63303bbd12ca884e724146135f181772a0a58dd7.

--- creatorwallet/main_app/models.py
+++ creatorwallet/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+# Import the User
+from django.contrib.auth.models import User
 
-# Create your models here.
+# # Create your models here.
+# class Cat(models.Model):
+#   ...
+#   # toys = models.ManyToManyField(Toy)
+#   # Add the foreign key linking to a user instance
+#   user = models.ForeignKey(User, on_delete=models.CASCADE)
