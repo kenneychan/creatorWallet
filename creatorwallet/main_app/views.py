3856aeaf7a4a6168a257053a6c115eece386bd6b
Kenney Chan <kchan10@gmail.com>
2023-07-28 13:54:50 -0700

    Add user authentication

--- creatorwallet/main_app/views.py
+++ creatorwallet/main_app/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+# Import the login_required decorator
+from django.contrib.auth.decorators import login_required
+# Import the mixin for class-based views
+from django.contrib.auth.mixins import LoginRequiredMixin
+# from .models import Cat
 
 # Create your views here.
+# Define the home view
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
+
+# Define the about view
+def about(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'about.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+# class CatCreate(LoginRequiredMixin, CreateView):
+#   model = Cat
+  
+#   # This inherited method is called when a
+#   # valid cat form is being submitted
+#   def form_valid(self, form):
+#     # Assign the logged in user (self.request.user)
+#     form.instance.user = self.request.user  # form.instance is the cat
+#     # Let the CreateView do its job as usual
+#     return super().form_valid(form)
+  
+# @login_required
+# def cats_index(request):
+#   cats = Cat.objects.filter(user=request.user)
+#   # You could also retrieve the logged in user's cats like this
+#   # cats = request.user.cat_set.all()
+#   return render(request, 'cats/index.html', { 'cats': cats })
