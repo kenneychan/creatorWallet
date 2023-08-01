5c7e009bed7c6ed13de9e9d0b282bf084cf97637
Darius Bowser <134982506+DariusBowser@users.noreply.github.com>
2023-08-01 16:56:24 -0400

    added links to view platform(s)

--- creatorwallet/main_app/admin.py
+++ creatorwallet/main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # import your models here
-from .models import Deal
+from .models import Deal, Platform
 
 # Register your models here.
 admin.site.register(Deal)
+admin.site.register(Platform)
