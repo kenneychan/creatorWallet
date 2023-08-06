f980c6602a931b8595049e4a2c07669563cce8c8
Kenney Chan <kchan10@gmail.com>
2023-08-06 13:28:02 -0700

    checkpoint

--- creatorwallet/creatorwallet/settings.py
+++ creatorwallet/creatorwallet/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ
 
-environ.Env()
+env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,12 +81,39 @@ WSGI_APPLICATION = 'creatorwallet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#   'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'neondb',
+#     'USER': 'kenneychan',
+#     'PASSWORD': 'UClPra4x5YZS',
+#     'HOST': 'ep-patient-fire-31937482.us-west-2.aws.neon.tech',
+#     'PORT': '5432',
+#   }
+# }
+# SECRET_KEY= env('SECRET_KEY')
+
+# DATABASE_NAME='neondb'
+# DATABASE_USER='kenneychan'
+# DATABASE_PASS='UClPra4x5YZS'
+# DATABASE_HOST='ep-patient-fire-31937482.us-west-2.aws.neon.tech'
+# DATABASE_PORT='5432'
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('DATABASE_NAME'),
+#         'USER': env('DATABASE_USER'),
+#         'PASSWORD': env('DATABASE_PASS'),
+#         'HOST': env('DATABASE_HOST'),
+#         'PORT': env('DATABASE_PORT')
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'creatorwallet',
-        'USER': 'postgres',
-        'PASSWORD': '12345'
     }
 }
 
