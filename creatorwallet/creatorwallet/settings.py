4971a5b342297d59e48b438ffb485a1f42342bc8
Kenney Chan <kchan10@gmail.com>
2023-09-28 18:21:11 -0700

    Revert "test allowed_hosts"
    This reverts commit 9899271db816c5a94b9871900ccf5969ec15f82c.

--- creatorwallet/creatorwallet/settings.py
+++ creatorwallet/creatorwallet/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY= env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000','creatorwallet2-f02c1911c973.herokuapp.com']
 
 # Application definition
 
