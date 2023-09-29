93b9e53dea411939764f286e35db6df5d58040d0
Kenney Chan <kchan10@gmail.com>
2023-09-28 18:00:33 -0700

    Update ALLOWED_HOSTS

--- creatorwallet/creatorwallet/settings.py
+++ creatorwallet/creatorwallet/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY= env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1:8000', env('DJANGO_ALLOWED_HOSTS')]
 
 # Application definition
 
