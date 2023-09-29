ab4c2cdfbdfe9d4acdc47ceea95919a522084e81
Kenney Chan <kchan10@gmail.com>
2023-09-28 18:54:52 -0700

    add staticfiles

--- creatorwallet/creatorwallet/settings.py
+++ creatorwallet/creatorwallet/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 
@@ -66,6 +66,9 @@ LOGGING = {
         }
     }
 }
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 # Application definition
 
@@ -166,6 +169,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/dashboard'
 LOGOUT_REDIRECT_URL = '/'
---
+++ creatorwallet/staticfiles/javascripts/script.b69e6ac27f5f.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+  $('.sidenav').sidenav({
+    edge: 'right'
+  });
+  if (isTouchDevice() === false) {
+    $('.tooltipped').tooltip();
+  }
+  $('.fixed-action-btn').floatingActionButton();
+  $('.parallax').parallax();
+  $('.modal').modal();
+  $('.collapsible').collapsible();
+
+  $('.carousel').carousel({
+    fullWidth: true,
+    // dist: 0,
+    indicators: true,
+  });
+  setInterval(function () {
+    $('.carousel').carousel('next');
+  }, 5000);
+
+  $('#id_details').addClass('materialize-textarea');
+  $('#id_due_date').addClass('datepicker');
+  $('#id_date').addClass('datepicker');
+
+  const date = new Date($('#id_due_date').val());
+  $('.datepicker').datepicker({
+    autoClose: true,
+    // defaultDate: date,
+    setDefaultDate: true,
+  });
+
+  moveDealPlatformBtn();
+  $(window).scroll(function () {
+    moveDealPlatformBtn();
+  });
+
+  function moveDealPlatformBtn() {
+    if ($(document).scrollTop() > 100) {
+      $('.add-deal-btn-top').addClass('scale-out');
+      $('.add-deal-btn-top').removeClass('scale-in');
+      $('.add-deal-btn-bottom').addClass('scale-in');
+      $('.add-deal-btn-bottom').addClass('scale-out');
+
+      $('.add-platform-btn-top').addClass('scale-out');
+      $('.add-platform-btn-top').removeClass('scale-in');
+      $('.add-platform-btn-bottom').addClass('scale-in');
+      $('.add-platform-btn-bottom').addClass('scale-out');
+    }
+    else {
+      $('.add-deal-btn-top').addClass('scale-in');
+      $('.add-deal-btn-top').removeClass('scale-out');
+      $('.add-deal-btn-bottom').addClass('scale-out');
+      $('.add-deal-btn-bottom').removeClass('scale-in');
+
+      $('.add-platform-btn-top').addClass('scale-in');
+      $('.add-platform-btn-top').removeClass('scale-out');
+      $('.add-platform-btn-bottom').addClass('scale-out');
+      $('.add-platform-btn-bottom').removeClass('scale-in');
+    }
+  }
+
+  function isTouchDevice() {
+    return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
+  }
+});
