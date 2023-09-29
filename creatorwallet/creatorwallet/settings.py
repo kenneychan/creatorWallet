baecfbbef2e880462aa503f677c6854da6676849
Kenney Chan <kchan10@gmail.com>
2023-09-28 18:22:18 -0700

    Revert "Set DEBUG=False, ALLOWED_HOSTS = [".herokuapp.com"]"
    This reverts commit bc81a2209bd7fccf6e00a0e3476d00c03ffee244.

--- creatorwallet/creatorwallet/settings.py
+++ creatorwallet/creatorwallet/settings.py
@@ -30,9 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY= env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [".herokuapp.com"]
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
